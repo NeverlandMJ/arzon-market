ba2c0ab568a0cbb39516d1a499f9849641b9bf59
NeverlandMJ <khasanovasumbula@gmail.com>
2022-07-26 13:33:47 +0500

    getting env port

--- go.mod
+++ go.mod
@@ -8,4 +8,4 @@ require (
 	github.com/lib/pq v1.10.6
 )
 
-require golang.org/x/crypto v0.0.0-20220722155217-630584e8d5aa // indirect
+require golang.org/x/crypto v0.0.0-20220722155217-630584e8d5aa
--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
-	"net/http"
 	"arzon-market/postgres"
 	"arzon-market/server"
+	"fmt"
+	"net/http"
+	"os"
 
 	_ "github.com/lib/pq"
 )
@@ -17,5 +19,16 @@ func main() {
 	repo := postgres.NewPostgresRepository(db)
 	r := server.NewRouter(repo)
 
-	http.ListenAndServe(":8080", r)
+	http.ListenAndServe(GetPort(), r)
+}
+
+// Get the Port from the environment so we can run on Heroku
+func GetPort() string {
+	var port = os.Getenv("PORT")
+	// Set a default port if there is nothing in the environment
+	if port == "" {
+		port = "8080"
+		fmt.Println("INFO: No PORT environment variable detected, defaulting to " + port)
+	}
+	return ":" + port
 }
